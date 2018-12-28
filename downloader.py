7d7c00065a89281c58b7ae10f8bded33e2681228
Peter <me@peterhames.com>
2018-12-28 02:59:01 -0800

    Tweaks to print() in downloader.py

--- downloader.py
+++ downloader.py
@@ -35,18 +35,18 @@ def get_download_list(schedule_dict, rec_start_time, rec_end_time, audio_dir):
             print(file_path)
             exists = os.path.isfile(file_path)
             if exists:
-                print('File already downloaded:',pid)
+                print('File already downloaded:'+pid)
             else:
                 download_list.append(pid)
-                print('Added to download list:', start_time, end_time, schedule_dict[key]['NAME'], pid)
-    print(len(download_list), 'programs to download:', download_list)
+                print('Added to download list:'+start_time+' '+end_time+' '+schedule_dict[key]['NAME']+' '+pid)
+    print(len(download_list)+' programs to download: '+download_list)
     return download_list
 
 def init_download(download_list, audio_dir):
 # Tell get_iplayer to record PIDs
     download_str = ','.join(download_list)
     for path in sh.execute(['get_iplayer', '--type=radio', '--pid='+download_str, '--file-prefix=<pid>', '--radiomode=good', '--output='+audio_dir, '--force', '--overwrite']):
-        print(path,)
+        print(path)
     # TO DO: Add error/output logging
     return
 
@@ -61,7 +61,7 @@ def downloader(hours_ahead, audio_dir, json_dir):
     raw_schedule_dict = sh.load_json(year, month, day,json_dir)
     print('JSON imported')
     schedule_dict = sh.convert_dict_dates(raw_schedule_dict)
-    print('Dict times converted:' ,len(schedule_dict), 'records')
+    print('Dict times converted: '+len(schedule_dict)+' records')
     download_list = get_download_list(schedule_dict, rec_start_time, rec_end_time, audio_dir)
     print('Download list compiled')
     init_download(download_list, audio_dir)
