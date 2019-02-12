771f16ad8000764f25363fedf8f862ae5efc95af
Peter <me@peterhames.com>
2019-02-11 18:58:50 -0800

    Adding test logging code to main.py

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import scraper
 import downloader
 import shared as sh
 import datetime as dt
+import logging
 
 audio_dir = 'audio/'
 json_dir = 'json/'
 hours_ahead = 4
 
+logging.basicConfig(filename='logging_test.log', format='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
 scraper.scraper(json_dir)
-print("MAIN: SCRAPER COMPLETE")
+logging.info("Scraper complete")
 sh.cleanup(audio_dir,12)
 print("MAIN: AUDIO CLEANUP COMPLETE")
 sh.cleanup(json_dir,48)
